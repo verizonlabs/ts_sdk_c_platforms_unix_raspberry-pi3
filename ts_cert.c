8f4b571e2ea2a550f106cd9a2c37b1f4c7f24a66
Larry Ciummo <larry.ciummo@verizonc.com>
2018-08-30 16:59:11 -0400

    temp version for PP usage

--- ts_cert.c
+++ ts_cert.c
@@ -339,6 +339,9 @@ TsStatus_t ts_scepconfig_save( TsScepConfigRef_t* pConfig, char* path, char* fil
 	 	ts_file_handle handle;
 	 	uint32_t actual_size, size;
 	 	uint8_t* addr;
+	 	char line[120];
+	 	char conv[100];
+
 
 	 	// Set the default directory, then open and size the file. Malloc some ram and read it all it.
 
@@ -355,9 +358,284 @@ TsStatus_t ts_scepconfig_save( TsScepConfigRef_t* pConfig, char* path, char* fil
 	   // Read each line in the config, storing the data, but first verify the format written is compatible with this
 	   // version of the code
 
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+
+	 	// Go the REV = check it - error if no match
+	 	if (strcmp(text_line,SCEP_CONFIG_REV ) !=0)
+	 	{
+	 		iret = TsStatusErrorMediaInvalid;
+	 		goto error;
+
+	 	}
+
+	 	// Auto Renew
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	 	pConfig->_enabled = (strncmp(text_line,"1")==0)?true:false;
+
+	 	// Generate private key
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	 	pConfig->_generateNewPrivateKey = (strncmp(text_line,"1")==0)?true:false;
+
+	 	// _certExpiresAfter
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	    sscanf( text_line, "%d", pConfig->_certExpiresAfter);
+
+	    // _certEnrollmentType
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	    sscanf( text_line, "%d", pConfig->_certEnrollmentType);
+
+        // _numDaysBeforeAutoRenew
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	    sscanf( text_line, "%d", pConfig->_numDaysBeforeAutoRenew);
+
+	    // _encryptionAlgorithm
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	 	// Check for null param pointer
+	 	if (pConfig->_encryptionAlgorithm!=0) {
+	        sscanf( text_line, "%s", pConfig->_encryptionAlgorithm);
+	 	}
+	 	else
+	 	{
+	 		iret = TsStatusErrorPayloadTooLarge;
+	 		goto error;
+	 	}
+
+	 	// _hashFunction
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	 	// Check for null param pointer
+	 	if (pConfig->_hashFunction!=0) {
+	        sscanf( text_line, "%s", pConfig->_hashFunction);
+	 	}
+	 	else
+	 	{
+	 		iret = TsStatusErrorPayloadTooLarge;
+	 		goto error;
+	 	}
+
+	 	// _retries
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	    sscanf( text_line, "%d", pConfig->_retries);
+
+	    // _retryDelayInSeconds
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	    sscanf( text_line, "%d",pConfig->_retryDelayInSeconds);
+
+	    // _keySize
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	    sscanf( text_line, "%d", pConfig->_keySize);
+
+	    // _keyUsage
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	 	// Check for null param pointer
+	 	if (pConfig->_hashFunction!=0) {
+	        sscanf( text_line, "%s", pConfig->_keyUsage);
+	 	}
+	 	else
+	 	{
+	 		iret = TsStatusErrorPayloadTooLarge;
+	 		goto error;
+	 	}
+
+	 	// _keyAlgorithm
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	 	// Check for null param pointer
+	 	if (pConfig->_hashFunction!=0) {
+	        sscanf( text_line, "%s", pConfig->_keyAlgorithm);
+	 	}
+	 	else
+	 	{
+	 		iret = TsStatusErrorPayloadTooLarge;
+	 		goto error;
+	 	}
+
+	 	// _keyAlgorithmStrength
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	 	// Check for null param pointer
+	 	if (pConfig->_hashFunction!=0) {
+	        sscanf( text_line, "%s", pConfig->_keyAlgorithmStrength);
+	 	}
+	 	else
+	 	{
+	 		iret = TsStatusErrorPayloadTooLarge;
+	 		goto error;
+	 	}
+
+	 	// _caInstance
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	    sscanf( text_line, "%d", pConfig->_caInstance);
+
+	 	// _challengeType
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	    sscanf( text_line, "%d", pConfig->_challengeType);
+
+	    // _challengeUsername
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	 	// Check for null param pointer
+	 	if (pConfig->_hashFunction!=0) {
+	        sscanf( text_line, "%s", pConfig->_challengeUsername);
+	 	}
+	 	else
+	 	{
+	 		iret = TsStatusErrorPayloadTooLarge;
+	 		goto error;
+	 	}
+
+	 	// _challengePassword
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	 	// Check for null param pointer
+	 	if (pConfig->_hashFunction!=0) {
+	        sscanf( text_line, "%s", pConfig->_challengePassword);
+	 	}
+	 	else
+	 	{
+	 		iret = TsStatusErrorPayloadTooLarge;
+	 		goto error;
+	 	}
+
+	 	// _caCertFingerprint
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	 	// Check for null param pointer
+	 	if (pConfig->_hashFunction!=0) {
+	        sscanf( text_line, "%s", pConfig->_caCertFingerprint);
+	 	}
+	 	else
+	 	{
+	 		iret = TsStatusErrorPayloadTooLarge;
+	 		goto error;
+	 	}
+
+	 	// _certSubject
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	 	// Check for null param pointer
+	 	if (pConfig->_hashFunction!=0) {
+	        sscanf( text_line, "%s", pConfig->_certSubject);
+	 	}
+	 	else
+	 	{
+	 		iret = TsStatusErrorPayloadTooLarge;
+	 		goto error;
+	 	}
+
+	 	// _getCaCertUrl
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	 	// Check for null param pointer
+	 	if (pConfig->_hashFunction!=0) {
+	        sscanf( text_line, "%s", pConfig->_getCaCertUrl);
+	 	}
+	 	else
+	 	{
+	 		iret = TsStatusErrorPayloadTooLarge;
+	 		goto error;
+	 	}
+
+        // _getCaCertUrl
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	    sscanf( text_line, "%d", pConfig->_getCaCertUrl);
+
+	    // _getCertInitialUrl
+	    iret = ts_file_readline(&handle, text_line, sizeof(text_line));
+	 	if (TsStatusOk != iret)
+	 		goto error;
+	    sscanf( text_line, "%d", pConfig->_getCertInitialUrl);
+
+#if 0
+		bool _enabled;			// Specifies if certificate auto-renewal is enable/disable on the device
+		bool _generateNewPrivateKey;
+		int _certExpiresAfter;		// Specifies the datetime, in ISO 8601 format, when this certificates expires.
+		int _certEnrollmentType;	// Specifies the protocol used to distribute the device certificate(SCEP/EST)
+		int _numDaysBeforeAutoRenew;	// Specifies the number of days before expiration where the certificate should be renewed
+		char *_encryptionAlgorithm 100;  	// Specifies the encryption algorithm RSA DE
+		char *_hashFunction 16; 		// Specifies the hash function to be used
+		int _retries; 			// Specifies the number of times to retries a cert renewal.
+		int _retryDelayInSeconds; 	// Specifies the delay, in units of seconds, between retries
+		int _keySize; 			// Specifies the key size
+		char *_keyUsage 10; 			// Arrays of strings that specifies the key usage values
+		char *_keyAlgorithm 100; 		// Specifies the key algorithm
+		char *_keyAlgorithmStrength 4096 2048 256 512; 	// Specifies the key algorithm strength
+		int _caInstance;
+		int _challengeType;
+		char *_challengeUsername 20;
+		char* urlBuffer = "XXXXXXXXXXXXXXXX";
+		char __challengeUsername = &urlBuffer;
+		char *_challengePassword; 20
+		char *_caCertFingerprint; 100
+		char *_certSubject; 100
+		char *_getCaCertUrl; 100
+		int _getCaCertUrl;
+		int _getCertInitialUrl;
 
 
-
+		ts_status_debug("ts_scepconfig_create");
+		ts_platform_assert(scepconfig != NULL);
+		*scepconfig = (TsScepConfigRef_t)ts_platform_malloc(sizeof(TsScepConfig_t));
+		(*scepconfig)->_enabled = false;
+		(*scepconfig)->_certExpiresAfter = false;
+		(*scepconfig)->_certEnrollmentType = false;
+		(*scepconfig)->_numDaysBeforeAutoRenew = 0;
+		(*scepconfig)->_encryptionAlgorithm = 1000;
+		(*scepconfig)->_hashFunction = 15;
+		(*scepconfig)->_retries = 0;
+		(*scepconfig)->_retryDelayInSeconds = 1000;
+		(*scepconfig)->_keySize = 15;
+		(*scepconfig)->_keyUsage = 0;
+		(*scepconfig)->_keyAlgorithm = 1;
+		(*scepconfig)->_keyAlgorithmStrength = 0;
+		(*scepconfig)->_caInstance = 1;
+		(*scepconfig)->_challengeType = 0;
+		char buffer[1000];
+		(*scepconfig)->_challengeUsername = 1000;
+		(*scepconfig)->_challengePassword = 15;
+		(*scepconfig)->_caCertFingerprint = 0;
+		(*scepconfig)->_certSubject = 1;
+		(*scepconfig)->_getCaCertUrl = 1000;
+		(*scepconfig)->_getPkcsRequestUrl = 15;
+		(*scepconfig)->_getCertInitialUrl = 0;
+		(*scepconfig)->_messageCallback = messageCallback;
+#endif
 
 
 	 	ts_file_close(&handle);
@@ -368,6 +646,7 @@ TsStatus_t ts_scepconfig_save( TsScepConfigRef_t* pConfig, char* path, char* fil
 
   }
 
+#warning "\n\nDELETE BEFORE MERGE\n\n\n"
 #if 0
  // Loads a crypto object into memory from a files. Sizes the file and malloc needed memory
  // Certificate storage and keys - base credentials
@@ -413,4 +692,144 @@ TsStatus_t ts_scepconfig_save( TsScepConfigRef_t* pConfig, char* path, char* fil
  	return iret;
 
  }
+
+
+	// Delete a directory that doesn't exist - ERROR
+	iret = ts_file_directory_delete(TDIR_NAME);
+	printf("dir delete retruns error %d\n\r", iret);
+
+	// Create  a directory - TEST
+	iret = ts_file_directory_create(TDIR_NAME);
+	printf("dir create returns error %d\n\r", iret);
+
+#ifdef DELETE_TDIR
+	// Delete the directory just created
+	iret = ts_file_directory_delete(TDIR_NAME);
+	printf("dir delete retruns error %d\n\r", iret);
+#endif
+	// Get the current default directory
+	iret = ts_file_directory_default_set(TDIR_NAME);
+	printf("dir default SET returns  error %d\n\r", iret);
+
+
+
+#if 0
+	// Get the current default directory
+	strcpy(name,"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+	iret = ts_file_directory_default_get(&namePtr);
+	printf("dir default get returns  error %d \n\r", iret);
+
+#endif
+	// Delete the test file in case its there
+	iret = ts_file_delete(TFILE_NAME);
+	printf("DELETE test file before write  error %d - Current default dir is now...\n\r", iret);
+
+
+	// Create a test file
+	iret =  ts_file_create(TFILE_NAME);
+	printf("Create file  returns  error %d..\n\r", iret);
+
+
+
+	// Open a file for writing
+	iret =  ts_file_open(&handle, TFILE_NAME, TS_FILE_OPEN_FOR_WRITE);
+	printf("Open file  returns  error %d..\n\r", iret);
+
+
+	// Write some lines
+	iret = ts_file_write(&handle,"12345678\n\r", 10);
+	printf("WRITE 1 file  returns  error %d..\n\r", iret);
+
+
+
+	iret = ts_file_write(&handle,"abcdefgh\n\r", 10);
+	printf("WRITE 2 file  returns  error %d..\n\r", iret);
+
+
+	iret = ts_file_close(&handle);
+	printf("First CLOSE  error %d..\n\r", iret);
+
+
+	iret = ts_file_close(&handle);
+	printf("SECOND CLOSE  error %d..\n\r", iret);
+
+
+	// Open the file for reading
+	iret =  ts_file_open(&handle, TFILE_NAME, TS_FILE_OPEN_FOR_READ);
+	printf("Open file  for READ error %d..\n\r", iret);
+
+	// Size the file
+	actualRead=0;
+	iret = ts_file_size(&handle, &actualRead);
+	printf("SIZE 1 file  returns  error %d  LENGTH size %d..\n\r", iret, actualRead);
+	// Read a couple of line from it
+	actualRead=0;
+	iret = ts_file_read(&handle,readbuf, 100, &actualRead);
+	printf("READ 1 file  returns  error %d  LENGTH read %d..\n\r", iret, actualRead);
+	readbuf[actualRead+1]=0; // end of string in case binara
+	printf("READ data >>%s<<\n\r", readbuf);
+
+
+
+	// Seek pack to 0
+	iret = ts_file_seek(&handle,0);
+	printf("SEEK error %d..\n\r", iret);
+
+	// Read the first line again
+	iret = ts_file_read(&handle,readbuf, 10, &actualRead);
+
+	printf("READ after seek file  returns  error %d  LENGTH read %d..\n\r", iret, actualRead);
+	readbuf[actualRead+1]=0; // end of string in case binara
+
+	printf("READ data after seek >>%s<<\n\r", readbuf);
+
+
+    // Read an existing file by line
+	iret = ts_file_close(&handle);
+
+   ts_file_directory_default_set("..");  // up from subdir
+	iret =  ts_file_open(&handle, "line.txt", TS_FILE_OPEN_FOR_READ);
+
+    iret = TsStatusOk;
+    char text_line[3];
+    while(iret==TsStatusOk) {
+
+      iret = ts_file_readline(&handle,text_line, sizeof(text_line));
+      printf("Line read status %d len %d>>>%s<\n",iret, strlen(text_line),text_line);
+    sleep(1);
+
+    }
+
+
+
+     // Close the file
+
+	iret = ts_file_close(&handle);
+
+
+     // Writeline test
+	iret =  ts_file_create("newline.txt");
+	printf("Create file  returns  error %d..\n\r", iret);
+	iret =  ts_file_open(&handle, "newline.txt", TS_FILE_OPEN_FOR_WRITE);
+      iret = ts_file_writeline(&handle,"Line 1\n");
+      iret = ts_file_writeline(&handle,"Line 222222\n");
+      iret = ts_file_writeline(&handle,"Line 3\n");
+	iret = ts_file_close(&handle);
+
+
+
+
+
+
+
+	ts_file_assert(0);
+	/* Program should not reach beyond the assert(0). */
+
+
+
+
+
+	return 0;
+}
+
 #endif
