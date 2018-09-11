ae9964a0b971e1d2589ea051963e187316ef3180
Larry Ciummo <larry.ciummo@verizon.com>
2018-09-10 20:14:41 -0400

    messed up ts scep

--- ts_scep.c
+++ ts_scep.c
@@ -39,22 +39,21 @@ typedef struct fx_errors_description
 
 const     fx_error_codes fx_error_codes_array[] =
 {
-#warning "boil this down"
     // First column is native error code, 2nd column is TS error mapped to
     {0x00, TsStatusOk},
     //{0x01, TsStatusErrorBootError},
 
 
 	// Keep this flag last
-    {0xFFFF, 0xFF},
+    {0xFFFF, 0xFF}
 
 };
 
-static void             ts_initialize();
-static TsStatus_t ts_scep_enroll(TsScepConfigRef_t ptrScepConfig){
+static void   ts_initialize();
+static TsStatus_t ts_enroll(TsScepConfigRef_t config, scepOpType op);
 
 
-static void             ts_assertion(const char *msg, const char *file, int line);
+static void    ts_assertion(const char *msg, const char *file, int line);
 
 
 static TsFileVtable_t ts_platform_file = {
@@ -63,7 +62,7 @@ static TsFileVtable_t ts_platform_file = {
     .assertion = ts_assertion
 };
 
-const TsFileVtable_t * ts_file = &ts_platform_scep;
+const TsFileVtable_t * ts_scep = &ts_platform_scep;
 
 /**
  * Initialize the storage device (flash) and the file system
@@ -97,8 +96,9 @@ static TsStatus_t ts_map_error(uint32_t scepError)
 }
 
 
-static TsStatus_t ts_scep_enroll(TsScepConfigRef_t ptrScepConfig){
-	
+static TsStatus_t ts_scep_enroll(TsScepConfigRef_t ptrScepConfig)
+{
+#warning "ts_Scep.c building <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
 }
 
  static void ts_assertion(const char *msg, const char *file, int line) {
